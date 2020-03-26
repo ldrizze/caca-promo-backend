98054d4f760871ef63da1276419586cbddd69f05
William <william.vecchini@gmail.com>
2020-03-25 22:55:09 -0300

    Atualização ModelDefine
    Adicionado os os Imports e Defines dos Models.

--- app/modelDefine.js
+++ app/modelDefine.js
@@ -1,7 +1,19 @@
 module.exports = (sequelize) => {
   // Imports
   const UsuarioModelDefine = require('./models/Usuario').define
+  const RestauranteModelDefine = require('./models/Restaurante').define
+  const PromocaoModelDefine = require('./models/Promocao').define
+  const AvaliacaoModelDefine = require('./models/Avaliacao').define
+  const ContatoModelDefine = require('./models/Contatos').define
+  const TipoComidaModelDefine = require('./models/TipoComida').define
+  const ComidaFavoritaModelDefine = require('./models/ComidaFavorita').define
 
   // Defines
   UsuarioModelDefine(sequelize)
+  RestauranteModelDefine(sequelize)
+  PromocaoModelDefine(sequelize)
+  AvaliacaoModelDefine(sequelize)
+  ContatoModelDefine(sequelize)
+  TipoComidaModelDefine(sequelize)
+  ComidaFavoritaModelDefine(sequelize)
 }
